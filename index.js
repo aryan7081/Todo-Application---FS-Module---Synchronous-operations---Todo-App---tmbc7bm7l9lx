b6196e56ee8dcccca672d94a9eb8bc37d065394d
aryan7081 <haryan458@gmail.com>
2024-08-19 17:00:43 +0530

    Updated all functions

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-const getTodosSync = () => {};
+const { json } = require("express");
+const fs = require("fs")
 
-const getTodoSync = (id) => {};
 
-const createTodoSync = (todo) => {};
+const getTodosSync = () => {
+  return fs.readFileSync("db.txt","utf-8")
+};
 
-const updateTodoSync = (id, updates) => {};
+const getTodoSync = (id) => {
+  const data = fs.readFileSync("db.txt","utf-8").split("\n")
+  for (let i = 0; i<data.length; i++){
+    const res = JSON.parse(data[i])
+    if (res.id === id){
+      return JSON.stringify(res)
+      
+    }
 
-const deleteTodoSync = (id) => {};
+  }
+};
+
+const createTodoSync = (todo) => {
+  const todoData = {
+    id: Date.now(),
+    title: todo,
+    isCompleted: false,
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  }
+  const data = `${JSON.stringify(todoData)}`
+  fs.writeFileSync("db.txt", `${data}\n`, { flag: 'a' });
+};
+
+const updateTodoSync = (id, updates) => {
+  const existingBef = fs.readFileSync("db.txt","utf-8")
+  const existing = fs.readFileSync("db.txt","utf-8").split("\n")
+  
+  for (let i = 0; i<existing.length; i++){
+    const res = JSON.parse(existing[i])
+    if (res.id == id){
+      let updatedTodo = JSON.stringify({...res,...updates,updatedAt: new Date().toISOString()})
+      const replaced = existingBef.replace(existing[i],updatedTodo)
+      fs.writeFileSync("db.txt",replaced)
+    }
+
+  }
+};
+
+const deleteTodoSync = (id) => {
+  const existingBef = fs.readFileSync("db.txt","utf-8")
+  const existing = fs.readFileSync("db.txt","utf-8").split("\n")
+  
+  for (let i = 0; i<existing.length; i++){
+    const res = JSON.parse(existing[i])
+    if (res.id == id){
+      const replaced = existingBef.replace(existing[i],"")
+      fs.writeFileSync("db.txt",replaced)
+    }
+
+  }
+};
+
 
 module.exports = {
   getTodosSync,
